78c9064e50f6ef326ff4008db5b7bd3136c3dda8
Enrique García <a_luis_e@hotmail.com>
2017-09-01 17:51:18 -0500

    Práctica 3

---
+++ practica1/main_prac3.cpp
@@ -0,0 +1,244 @@
+//Semestre 2018 - 1
+//************************************************************//
+//************************************************************//
+//************** Alumno (s): *********************************//
+//*************	GARCÍA ROSALES LUIS ENRIQUE				******//
+//*************		Grupo 1								******//
+//************************************************************//
+
+//#include <gl/gl.h>     // The GL Header File
+//#include <GL/glut.h>   // The GL Utility Toolkit (Glut) Header
+//#include <stdlib.h>
+#include "Main.h"
+
+float angleX = 0.0f;
+float angleY = 0.0f;
+float angleZ = 0.0f;
+float transX = 0.0f;
+float transY = 0.0f;
+float transZ = -5.0f;
+
+GLfloat c1[3] = { 0.83f, 0.02f, 0.02f }; //rojo
+GLfloat c2[3] = { 1.0f, 0.45f, 0.2f }; //naranja
+GLfloat c3[3] = { 1.0f, 0.94f, 0.2f }; //amarillo
+GLfloat c4[3] = { 0.0f, 0.45f, 0.23f }; //verde
+GLfloat c5[3] = { 0.0f, 0.32f, 0.65f }; //azul
+GLfloat c6[3] = { 0.4f, 0.12f, 0.5f }; //morado
+GLfloat c7[3] = { 1.0f, 0.7f, 0.8f }; //rosa
+GLfloat c8[3] = { 0.67f, 0.86f, 0.86f }; //aguamarina
+GLfloat c9[3] = { 1.0f, 1.0f, 1.0f }; //blanco
+GLfloat c10[3] = { 0.85f, 0.61f, 0.12f }; //oro
+
+
+void InitGL ( GLvoid )     // Inicializamos parametros
+{
+
+	//glShadeModel(GL_SMOOTH);							// Habilitamos Smooth Shading
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);				// Negro de fondo
+	glClearDepth(1.0f);									// Configuramos Depth Buffer
+	glEnable(GL_DEPTH_TEST);							// Habilitamos Depth Testing
+	//glEnable(GL_LIGHTING);
+	glDepthFunc(GL_LEQUAL);								// Tipo de Depth Testing a realizar
+	//glEnable ( GL_COLOR_MATERIAL );
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+}
+
+void prisma(GLfloat colSup[3], GLfloat colInf[3], GLfloat colIzq[3], GLfloat colDer[3], GLfloat colAnt[3], GLfloat colPos[3], GLfloat base, GLfloat altura, GLfloat prof)
+{
+	GLfloat vertice [8][3] = {
+				{0.5 ,-0.5, 0.5},    //Coordenadas Vértice 0 V0
+				{-0.5 ,-0.5, 0.5},    //Coordenadas Vértice 1 V1
+				{-0.5 ,-0.5, -0.5},    //Coordenadas Vértice 2 V2
+				{0.5 ,-0.5, -0.5},    //Coordenadas Vértice 3 V3
+				{0.5 ,0.5, 0.5},    //Coordenadas Vértice 4 V4
+				{0.5 ,0.5, -0.5},    //Coordenadas Vértice 5 V5
+				{-0.5 ,0.5, -0.5},    //Coordenadas Vértice 6 V6
+				{-0.5 ,0.5, 0.5},    //Coordenadas Vértice 7 V7
+				};
+
+		glPushMatrix();
+		glScalef(base, altura, prof);
+
+		glBegin(GL_POLYGON);	//Cara anterior
+			glColor3fv(colAnt);
+			glVertex3fv(vertice[0]);
+			glVertex3fv(vertice[4]);
+			glVertex3fv(vertice[7]);
+			glVertex3fv(vertice[1]);
+		glEnd();
+
+		glBegin(GL_POLYGON);	//Cara posterior
+		glColor3fv(colPos);
+		glVertex3fv(vertice[6]);
+		glVertex3fv(vertice[5]);
+		glVertex3fv(vertice[3]);
+		glVertex3fv(vertice[2]);
+		glEnd();
+
+		glBegin(GL_POLYGON);	//Cara derecha
+		glColor3fv(colDer);
+			glVertex3fv(vertice[0]);
+			glVertex3fv(vertice[3]);
+			glVertex3fv(vertice[5]);
+			glVertex3fv(vertice[4]);
+		glEnd();
+
+
+		glBegin(GL_POLYGON);  //Cara izquierda
+			glColor3fv(colIzq);
+			glVertex3fv(vertice[1]);
+			glVertex3fv(vertice[7]);
+			glVertex3fv(vertice[6]);
+			glVertex3fv(vertice[2]);
+		glEnd();
+
+		glBegin(GL_POLYGON);  //Cara inferior
+	
+		glColor3fv(colInf);
+			glVertex3fv(vertice[0]);
+			glVertex3fv(vertice[1]);
+			glVertex3fv(vertice[2]);
+			glVertex3fv(vertice[3]);
+		glEnd();
+
+		glBegin(GL_POLYGON);  //Cara superior
+			glColor3fv(colSup);
+			glVertex3fv(vertice[4]);
+			glVertex3fv(vertice[5]);
+			glVertex3fv(vertice[6]);
+			glVertex3fv(vertice[7]);
+		glEnd();
+
+		glPopMatrix();
+}
+
+void display ( void )   // Creamos la funcion donde se dibuja
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	// Limiamos pantalla y Depth Buffer
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+		//Poner Código Aquí.
+
+	glTranslatef(transX, transY, transZ);
+	glRotatef(angleX, 1.0f, 0.0f, 0.0f);
+	glRotatef(angleY, 0.0f, 1.0f, 0.0f);
+	glRotatef(angleZ, 0.0f, 0.0f, 1.0f);
+
+	/*Para la función prisma se requieren 9 parámetros
+	Los primeros seis son para el color en cada cara. Es un vector de tres flotantes
+	que se corresponde a un color RGB. Hay 10 colores ya declarados.
+	Los últimos tres son para las dimensiones del prisma, en este orden:
+	base, altura y profundidad*/
+	prisma(c1, c2, c3, c4, c5, c6, 2.0f, 1.0f, 3.0f);
+
+	glutSwapBuffers();
+}
+
+void reshape ( int width , int height )   // Creamos funcion Reshape
+{
+  if (height==0)										// Prevenir division entre cero
+	{
+		height=1;
+	}
+
+	glViewport(0,0,width,height);	
+
+	glMatrixMode(GL_PROJECTION);						// Seleccionamos Projection Matrix
+	glLoadIdentity();
+
+	// Tipo de Vista
+	//glOrtho(-5,5,-5,5,0.1,20);	
+	glFrustum (-0.1, 0.1,-0.1, 0.1, 0.1, 50.0);
+}
+
+void keyboard ( unsigned char key, int x, int y )  // Create Keyboard Function
+{
+	switch ( key ) {
+		case 'w':
+		case 'W':
+			transZ +=0.2f;
+			printf("Posicion en Z: %f\n", transZ);
+			break;
+		case 's':
+		case 'S':
+			transZ -=0.2f;
+			printf("Posicion en Z: %f\n", transZ);
+			break;
+		case 'a':
+		case 'A':
+			transX -=0.2f;
+			break;
+		case 'd':
+		case 'D':
+			transX +=0.2f;
+			break;
+		case 'q':
+		case 'Q':
+			transY -= 0.2f;
+			break;
+		case 'e':
+		case 'E':
+			transY += 0.2f;
+			break;
+		case 27:        // Cuando Esc es presionado...
+			exit ( 0 );   // Salimos del programa
+		break;        
+		default:        // Cualquier otra
+		break;
+  }
+	glutPostRedisplay();
+}
+
+void arrow_keys ( int a_keys, int x, int y )  // Funcion para manejo de teclas especiales (arrow keys)
+{
+  switch ( a_keys ) {
+    case GLUT_KEY_UP:     // Presionamos tecla ARRIBA...
+		angleX += 1.0f;
+		break;
+      //glutFullScreen ( ); // Full Screen Mode
+      //break;
+    case GLUT_KEY_DOWN:               // Presionamos tecla ABAJO...
+		angleX -= 1.0f;
+		break;
+      //glutReshapeWindow ( 500, 500 ); // Cambiamos tamano de Ventana
+      //break;
+	case GLUT_KEY_LEFT:
+		angleY += 1.0f;
+		break;
+	case GLUT_KEY_RIGHT:
+		angleY -= 1.0f;
+		break;
+	case GLUT_KEY_PAGE_UP:
+		angleZ += 1.0f;
+		break;
+	case GLUT_KEY_PAGE_DOWN:
+		angleZ -= 1.0f;
+		break;
+    default:
+      break;
+  }
+  glutPostRedisplay();
+}
+
+
+int main ( int argc, char** argv )   // Main Function
+{
+  glutInit            (&argc, argv); // Inicializamos OpenGL
+  //glutInitDisplayMode (GLUT_RGBA | GLUT_SINGLE | GLUT_DEPTH); // Display Mode (Clores RGB y alpha | Buffer Sencillo )
+  glutInitDisplayMode (GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH); // Display Mode (Clores RGB y alpha | Buffer Doble )
+  glutInitWindowSize  (500, 500);	// Tamaño de la Ventana
+  glutInitWindowPosition (0, 0);	//Posicion de la Ventana
+  glutCreateWindow    ("Practica 3"); // Nombre de la Ventana
+  InitGL ();						// Parametros iniciales de la aplicacion
+  glutDisplayFunc     ( display );  //Indicamos a Glut función de dibujo
+  glutReshapeFunc     ( reshape );	//Indicamos a Glut función en caso de cambio de tamano
+  glutKeyboardFunc    ( keyboard );	//Indicamos a Glut función de manejo de teclado
+  glutSpecialFunc     ( arrow_keys );	//Otras
+  glutMainLoop        ( );          // 
+
+  return 0;
+}
+
+
+
